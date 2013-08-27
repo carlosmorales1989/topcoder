52f740d7653a92abb47c50aae5b1491f19bef38d
Carlos Fernando Morales <carlos1337@gmail.com>
2013-08-27 08:43:00 -0500

    Flower garden solution
    Final solution to the flower garden problem

--- Topcoder/src/FlowerGarden.java
+++ Topcoder/src/FlowerGarden.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 /**
- * 
+ * 2004 TCCC Online Round 1 500 pt problem
  * @author Asistente
  *
  */
@@ -20,50 +20,36 @@ public class FlowerGarden {
 		
 		Arrays.sort(flowers);	
 		
-		ArrayList<Integer> result = new ArrayList();
-		result.add(0);
-		for (int i = 1; i < height.length; i++) {			
+		ArrayList<Flower> result = new ArrayList();
+		result.add(flowers[flowers.length-1]);
+		for (int i = height.length-2; i >=0; i--) {
 			int bloomi = flowers[i].bloom;
 			int wilti = flowers[i].wilt;
 			int heighti = flowers[i].height;
 			boolean added = false;
 			for (int j = 0; j < result.size() && !added; j++) {
-				int k = result.get(j);
-				int bloomj = bloom[k];
-				int wiltj = wilt[k];
-				int heightj = height[j];
+				Flower f = result.get(j);
+				int bloomj = f.bloom;
+				int wiltj = f.wilt;
+				int heightj = f.height;
 				System.out.println("Comparo: "+bloomi+","+wilti+" con "+bloomj+","+wiltj);
-				if((bloomi < bloomj && wilti > bloomj) || (bloomi > bloomj && bloomi < wiltj)){
+				if((bloomi <= bloomj && wilti >= bloomj) || (bloomi >= bloomj && bloomi <= wiltj)){
 					System.out.println("Cruzan");
-					// Se cruzan
-					if(heightj < heighti){
-						System.out.println("Agrego de todas formas");
-						result.add(j,i);
+						result.add(j,flowers[i]);
 						added=true;
-					}
-				}else{
-					System.out.println("No cruzan");
-					result.add(j,i);
-					added=true;
-				}				
+				}	
 			}
 			if(!added)
-				result.add(i);			
+				result.add(flowers[i]);			
 		}
 		
 		int[] resultArray = new int[height.length];
 		for (int i = 0; i < result.size(); i++) {
-			resultArray[i] = height[result.get(i)];
+			resultArray[i] = result.get(i).height;
 		}
 		return resultArray;
 	}
 	
-	public static void main(String[] args) {
-		FlowerGarden fg = new FlowerGarden();
-		System.out.println(Arrays.toString(fg.getOrdering(new int[]{5,4,3,2,1}, 
-				new int[]{1,5,10,15,20}, 
-				new int[]{4,9,14,19,24})));
-	}
 	
 	class Flower implements Comparable<Flower>{
 		
